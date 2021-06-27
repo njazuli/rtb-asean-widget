3fbef60014ba3d46fe6deeaff0aaa76556e36be0
njazuli <78890281+njazuli-deriv@users.noreply.github.com>
2021-06-27 23:40:41 +0800

    moved the setinterval

--- banner/js/widget.js
+++ banner/js/widget.js
@@ -46,59 +46,59 @@ function receiveMessage(event) {
   //event.source.postMessage({ status: 'ok', message: 'woot'}, event.origin);
 
   function time(date) {
-    var interval = setInterval(() => {
-      var endtime = Date.parse(date);
-      var today = new Date().getTime();
-      var t = endtime - new Date().getTime();
+    var endtime = Date.parse(date);
+    var today = new Date().getTime();
+    var t = endtime - new Date().getTime();
 
-      var sec = Math.floor((t / 1000) % 60);
-      var min = Math.floor((t / 1000 / 60) % 60);
-      var hour = Math.floor((t / (1000 * 60 * 60)) % 24);
-      var days = Math.floor(t / (1000 * 60 * 60 * 24));
-      if (today >= endtime) {
-        $(".container-widget").empty();
-        clearInterval(interval);
-        return;
-      }
+    var sec = Math.floor((t / 1000) % 60);
+    var min = Math.floor((t / 1000 / 60) % 60);
+    var hour = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    if (today >= endtime) {
+      $(".container-widget").empty();
+      clearInterval(interval);
+      return;
+    }
 
-      $(".days")
-        .empty()
-        .append(
-          `<span class="date-text text-center">${("0" + days).substr(
-            -2
-          )}</span><span class="date-small-text text-center">Days</span>`
-        );
+    $(".days")
+      .empty()
+      .append(
+        `<span class="date-text text-center">${("0" + days).substr(
+          -2
+        )}</span><span class="date-small-text text-center">Days</span>`
+      );
 
-      $(".hours")
-        .empty()
-        .append(
-          `<span class="date-text text-center">${("0" + hour).substr(
-            -2
-          )}</span><span class="date-small-text text-center">Hours</span>`
-        );
+    $(".hours")
+      .empty()
+      .append(
+        `<span class="date-text text-center">${("0" + hour).substr(
+          -2
+        )}</span><span class="date-small-text text-center">Hours</span>`
+      );
 
-      $(".minutes")
-        .empty()
-        .append(
-          `<span class="date-text text-center">${("0" + min).substr(
-            -2
-          )}</span><span class="date-small-text text-center">Minutes</span>`
-        );
+    $(".minutes")
+      .empty()
+      .append(
+        `<span class="date-text text-center">${("0" + min).substr(
+          -2
+        )}</span><span class="date-small-text text-center">Minutes</span>`
+      );
 
-      $(".seconds")
-        .empty()
-        .append(
-          `<span class="date-text text-center">${("0" + sec).substr(
-            -2
-          )}</span><span class="date-small-text text-center">Seconds</span>`
-        );
-    }, 1000);
+    $(".seconds")
+      .empty()
+      .append(
+        `<span class="date-text text-center">${("0" + sec).substr(
+          -2
+        )}</span><span class="date-small-text text-center">Seconds</span>`
+      );
   }
 
   if (event.data.action == "widgetenddate") {
     console.log(JSON.stringify(event.data.data.date));
     var dateRelated = event.data.data.date;
-    time(dateRelated);
+    setInterval(() => {
+      time(dateRelated);
+    }, 1000);
   }
 }
 
